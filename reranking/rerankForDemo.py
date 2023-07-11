7c40b25fabdeee3c2c921617aedac48429c81cef
Huytranxuan <74607304+TranXuanHuy267@users.noreply.github.com>
2023-07-11 18:28:56 +0700

    reranking code

---
+++ reranking/rerankForDemo.py
@@ -0,0 +1,97 @@
+
+from huggingface_hub import hf_hub_download
+import torch.nn as nn
+from PIL import Image
+from torchvision import transforms
+import torch
+from transformers import ViTImageProcessor, ViTModel
+import torchvision.transforms as T
+
+
+class Initial:
+    def __init__(self, model="ShuffleNet"):
+        assert model in ['ViT', "ShuffleNet"]
+        if model == "ShuffleNet":
+            self.process = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ])
+            self.model = rerankingShuffleNet()
+            version = None
+            hf_hub_download(repo_id="Huy1432884/rerankingShuffleNet",
+                filename="model.bin", 
+                use_auth_token="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", 
+                local_dir="/kaggle/working/",
+                revision=version
+            )
+        elif model == "ViT":
+            self.process = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
+            self.topil = T.ToPILImage()
+            self.model = rerankingViT()
+            version = None
+            hf_hub_download(repo_id="Huy1432884/rerankingViT", 
+                filename="model.bin", 
+                use_auth_token="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", 
+                local_dir="/kaggle/working/",
+                revision=version
+            )
+
+        self.model.load_state_dict(torch.load("/kaggle/working/model.bin", map_location=torch.device('cpu')))
+        self.model.eval()
+        
+def reranking(image1, image2s, initial, model="ShuffleNet"):
+    assert model in ['ViT', "ShuffleNet"]
+    def merge_height(image1, image2):
+        new_image = Image.new("RGB", (224, 224), (250, 250, 250))
+        new_image.paste(image1,(0,0))
+        new_image.paste(image2,(112,0))
+        return new_image
+    
+    
+    img1 = transforms.Resize((224, 112))(image1)
+    merged_imgs = []
+    for image2 in image2s:
+        img2 = transforms.Resize((224, 112))(image2)
+        merged_img = merge_height(img1, img2)
+    
+        if model == 'ShuffleNet':
+            merged_img = initial.process(merged_img).unsqueeze(0)
+        elif model == "ViT":
+            trans = transforms.ToTensor()
+            merged_img = trans(merged_img)
+            merged_img = initial.process(initial.topil(merged_img), return_tensors="pt")["pixel_values"]
+        merged_imgs.append(merged_img)
+    merged_imgs = torch.cat(merged_imgs, dim=0)
+    output = initial.model(merged_imgs)
+    output = output.squeeze(1).tolist()
+    return output
+    
+class rerankingShuffleNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = torch.hub.load('pytorch/vision:v0.10.0', 'shufflenet_v2_x1_0', pretrained=False)
+        self.model.fc = nn.Linear(1024, 1)
+        self.act = nn.Sigmoid()
+        self.crit = nn.MSELoss()
+    
+    def forward(self, merged_imgs):
+        logits = self.model(merged_imgs)
+        output = self.act(logits)
+        return output
+
+class rerankingViT(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.extractor = ViTModel.from_pretrained('google/vit-base-patch16-224')
+        self.hidden_size = self.extractor.config.hidden_size
+        self.fc = nn.Linear(768, 1)
+        self.act = nn.Sigmoid()
+        self.crit = nn.MSELoss()
+    
+    def forward(self, merged_imgs):
+        embed = self.extractor(merged_imgs)[0].mean(dim=1)
+        logits = self.fc(embed)
+        output = self.act(logits)
+        return output
